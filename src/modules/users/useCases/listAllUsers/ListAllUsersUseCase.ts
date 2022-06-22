5492c417ccd1b20e78971ce24d23af7cc60d2193
Greg <gregoryflucena@gmail.com>
2022-06-22 08:22:38 -0300

    Tests done

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,22 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists");
+    }
+
+    if (!user.admin) {
+      throw new Error("You does not have permissions");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
