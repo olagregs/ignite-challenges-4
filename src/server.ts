86c75cc5faae3543d881331110f1969c5f3e716c
Greg <gregoryflucena@gmail.com>
2022-06-23 19:54:28 -0300

    Add swagger documentation

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import swagger from "swagger-ui-express";
+
 import { app } from ".";
+import swaggerFile from "./swagger.json";
+
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
